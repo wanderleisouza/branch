8c432f860ab1392867109bfb55acf2cefd27d1d5
Wanderlei Souza <wandi.darko@gmail.com>
2020-01-07 22:03:41 -0500

    Add lombok dependency

--- src/main/java/com/example/branch/domain/Branch.java
+++ src/main/java/com/example/branch/domain/Branch.java
@@ -3,8 +3,16 @@ package com.example.branch.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter @Setter @RequiredArgsConstructor
 public class Branch {
 	
 	@Id
@@ -13,5 +21,9 @@ public class Branch {
 	private String name;
 	private Double lon;	
 	private Double lat;
+	
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	@Transient
+	private Double distance;
 
 }
