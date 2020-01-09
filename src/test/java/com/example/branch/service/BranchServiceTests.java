3a15f74849460d9b0e6e4d136b477d6a2f736577
Wanderlei Souza <wandi.darko@gmail.com>
2020-01-09 19:01:51 -0200

    tests for find all service and find by id

--- src/test/java/com/example/branch/service/BranchServiceTests.java
+++ src/test/java/com/example/branch/service/BranchServiceTests.java
@@ -1,25 +1,26 @@
 package com.example.branch.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Arrays;
+import java.util.stream.StreamSupport;
+
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.branch.domain.Branch;
 import com.example.branch.exception.BranchNotFoundException;
 import com.example.branch.repository.BranchRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc
 public class BranchServiceTests {
 	
     @Autowired
@@ -28,14 +29,28 @@ public class BranchServiceTests {
     @MockBean
     private BranchRepository branchRepository;
     
-    @Before
-    public void setUp() {
-        Mockito.when(branchRepository.findById("ABC")).thenThrow(NullPointerException.class);
+    @Test
+    public void whenFindAllShouldReturnAllItems() {
+    	
+        Branch b1 = new Branch("-103", "VILA LEOPOLDINA", -46.728521, -23.530247, 0);
+        Branch b2= new Branch("-1281", "VILA LEOPOLDINA", -46.727677, -23.534168, 0);
+        Iterable<Branch> iterable = Arrays.asList(b1, b2);
+        
+        Mockito.reset(branchRepository);
+        Mockito.when(branchRepository.findAll()).thenReturn(iterable);
+        
+    	Iterable<Branch> all = branchService.findAll();
+    	long actual = StreamSupport.stream(all.spliterator(), false).count();
+    	long expected = StreamSupport.stream(iterable.spliterator(), false).count();
+    	assertEquals(expected, actual);
+    	
     }
     
     @Test
     public void whenInvalidIDthenBranchShouldBeNotFound() {
+        Mockito.when(branchRepository.findById("ABC")).thenThrow(NullPointerException.class);
+        Mockito.reset(branchRepository);
     	assertThrows(BranchNotFoundException.class, () -> branchService.findById("ABC"));
-     }
-   
+    }
+    
 }
